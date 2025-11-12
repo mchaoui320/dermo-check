d0a462d7f583378ce2f9ec4ad51be5a520c2cf30
mchaoui320 <chaoui.medissam@gmail.com>
2025-11-12 21:34:16 +0100

    fix: asset paths and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // important pour Vercel, mais marche aussi en local
-  server: {
-    port: 5173,
-  },
+  base: './', // ⚠️ INDISPENSABLE pour corriger la page blanche sur Netlify
 })
