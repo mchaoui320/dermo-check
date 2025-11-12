4b0daceb4b8fdd2d1c8bac61c17b12aa56e6e306
mchaoui320 <chaoui.medissam@gmail.com>
2025-11-12 20:59:31 +0100

    fix: adjust vite base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // important pour Vercel
+  base: '', // ⚠️ on retire le "./" pour Vercel
 })
