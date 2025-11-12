12345f8cf83fd7de99e22c3fcbc211cefa318c38
mchaoui320 <chaoui.medissam@gmail.com>
2025-11-12 21:13:16 +0100

    fix: vite public assets + favicon path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // ⚠️ on retire le "./" pour Vercel
+  base: '',            // chemins relatifs pour Vercel
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
